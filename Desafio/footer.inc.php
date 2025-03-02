1c3d788178eb05dd27b7a67484d34ea88eafa2fb
Arturo Duarte <arturduar60@gmail.com>
2025-03-01 21:53:14 -0600

    Cambio de nombre

--- Desafio/footer.inc.php
+++ Desafio/footer.inc.php
@@ -6,6 +6,6 @@
       <a href="#" class="text-white me-3 fs-2"><i class="fab fa-linkedin-in"></i></a>
       <a href="#" class="text-white fs-2"><i class="fab fa-instagram"></i></a>
     </div>
-    <p class="mb-0">&copy; <?php echo date("Y"); ?> Soluciones Innovadoras. Todos los derechos reservados.</p>
+    <p class="mb-0">&copy; <?php echo date("Y"); ?> SeguriTech. Todos los derechos reservados.</p>
   </div>
 </footer>
